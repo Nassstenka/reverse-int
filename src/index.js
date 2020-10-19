c15155fcf358398c67b52fe4254981cca6ad1b23
Anastasiya Satsiuk <nastyasatsyuk@yandex.by>
2020-10-19 23:10:24 +0300

    fix: make function work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let str = `${n}`;
+    let charArray = str.split('');
+    charArray.reverse();
+    str = charArray.join('');
+    return Number(str);
 }
